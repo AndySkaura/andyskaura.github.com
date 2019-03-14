d0e516e39af2af124f21714d42a4d555c9a65f5f
andy_skaura <andy_skaura@sina.com>
2019-03-14 16:47:16 +0800

    123

---
+++ js/gameMgr.js
@@ -0,0 +1,1 @@
+var demo;!function(n){var t=function(){function n(){}return n.getInstance=function(){return this._instance||(this._instance=new n),this._instance},n}();n.gameMgr=t}(demo||(demo={}));
