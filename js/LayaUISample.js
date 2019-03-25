e3ec239940e9803437d82979b81b318928ed3abf
andy_skaura <andy_skaura@sina.com>
2019-03-25 14:20:53 +0800

    add

--- js/LayaUISample.js
+++ js/LayaUISample.js
@@ -72,7 +72,7 @@ var TestUI = /** @class */ (function (_super) {
 }(ui.mainViewUI));
 //程序入口
 Laya.init(750, 1334, WebGL);
-Laya.stage.scaleMode = Laya.Stage.SCALE_EXACTFIT;
+Laya.stage.scaleMode = Laya.Stage.SCALE_NOSCALE;
 //激活资源版本控制
 Laya.ResourceVersion.enable("version.json", Handler.create(null, beginLoad), Laya.ResourceVersion.FILENAME_VERSION);
 function beginLoad() {
--- js/ui/layaUI.max.all.js
+++ js/ui/layaUI.max.all.js
@@ -24,7 +24,7 @@ var ui;
             _super.prototype.createChildren.call(this);
             this.createView(ui.mainViewUI.uiView);
         };
-        mainViewUI.uiView = { "type": "View", "props": { "width": 750, "height": 1334 }, "child": [{ "type": "Image", "props": { "top": 0, "skin": "comp/bg.png", "sizeGrid": "32,21,17,25", "right": 0, "left": 0, "bottom": 0 } }, { "type": "Image", "props": { "y": 0, "x": 290, "var": "role", "skin": "comp/image.png", "sizeGrid": "111,61,65,55", "bottom": 0 } }, { "type": "Animation", "props": { "y": 520, "x": 285, "var": "ani", "interval": 100 } }] };
+        mainViewUI.uiView = { "type": "View", "props": { "width": 750, "height": 1334 }, "child": [{ "type": "Image", "props": { "top": 0, "skin": "comp/bg.png", "sizeGrid": "32,21,17,25", "right": 0, "left": 0, "bottom": 0 } }, { "type": "Image", "props": { "y": 0, "x": 290, "var": "role", "skin": "comp/image.png", "sizeGrid": "111,61,65,55", "bottom": 0 } }, { "type": "Animation", "props": { "y": 520, "x": 285, "var": "ani", "interval": 100 } }, { "type": "Animation", "props": { "y": 224, "x": 71, "source": "lourouqiu/back_face_lourouqiu0.png,lourouqiu/back_face_lourouqiu1.png,lourouqiu/back_face_lourouqiu2.png,lourouqiu/back_face_lourouqiu3.png,lourouqiu/back_face_lourouqiu4.png,lourouqiu/face_lourouqiu0.png,lourouqiu/face_lourouqiu1.png,lourouqiu/go_face_lourouqiu0.png,lourouqiu/go_face_lourouqiu1.png,lourouqiu/go_face_lourouqiu2.png,lourouqiu/go_face_lourouqiu3.png,lourouqiu/go_face_lourouqiu4.png", "interval": 100, "autoPlay": true } }] };
         return mainViewUI;
     }(View));
     ui.mainViewUI = mainViewUI;
